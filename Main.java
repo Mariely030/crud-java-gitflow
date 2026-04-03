2f14c4437cbdd8e20613289bd15c908dc892d210
Rosse Mariely Mejia <rosse@Rosses-Mac-mini.local>
2026-04-02 22:18:34 -0400

    Cambio de  mostrarUsuarios

--- Main.java
+++ Main.java
@@ -50,8 +50,8 @@ public class Main {
     }
 
     public static void mostrarUsuarios() {
+        System.out.println("Mostrando usuarios...");
         for (int i = 0; i < listaUsuarios.size(); i++) {
-            System.out.println("Mostrando usuarios...");
             System.out.println(i + ": " + listaUsuarios.get(i));
         }
     }
